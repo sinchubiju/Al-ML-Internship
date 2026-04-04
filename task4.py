be3d00fc7d742742a5c418d53c382464a791b9d8
sinchubiju <sinchubijusinchubiju@gmail.com>
2026-04-04 21:39:06 +0530

    Day 2 python basics  completed

--- task4.py
+++ task4.py
@@ -1,25 +1,25 @@
-# def add(a, b):
-#     return a + b
+def add(a, b):
+    return a + b
 
-# x = 6
-# y = 4
+x = 6
+y = 4
 
-# print("Result:", add(x, y))
+print("Result:", add(x, y))
 
-# def is_prime(n):
-#     if n <= 1:
-#         return False
-#     for i in range(2, n):
-#         if n % i == 0:
-#             return False
-#     return True
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
-# num = 7
+num = 7
 
-# if is_prime(num):
-#     print("Prime")
-# else:
-#     print("Not Prime")
+if is_prime(num):
+    print("Prime")
+else:
+    print("Not Prime")
 
 def factorial(n):
     result = 1
