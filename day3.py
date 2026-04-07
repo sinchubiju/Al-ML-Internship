3acb26e35d3f2f5fec2967915224fc7c1ed8ec71
sinchubiju <sinchubijusinchubiju@gmail.com>
2026-04-07 17:14:24 +0530

    added new files and updated existing files

---
+++ day3.py
@@ -0,0 +1,18 @@
+numbers = [1, 5, 8, 12, 3, 7, 10, 15, 6, 9]
+print(numbers)
+
+numbers = [1, 5, 8, 12, 3, 7, 10, 15, 6, 9]
+print("Sum:", sum(numbers))
+
+numbers = [1, 5, 8, 12, 3, 7, 10, 15, 6, 9]
+print("Max:", max(numbers))
+
+numbers = [1, 5, 8, 12, 3, 7, 10, 15, 6, 9]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+        numbers = [1, 5, 8, 12, 3]
+numbers.reverse()
+print(numbers)
--- task4.py
+++ task4.py
@@ -22,7 +22,7 @@ else:
     print("Not Prime")
 
 def factorial(n):
-    result = 1
+    result 
     for i in range(1, n + 1):
         result *= i
     return result
