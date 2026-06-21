bb58f6979470c8813e2d2a55d32be705d1e1f58e
sinchubiju <sinchubijusinchubiju@gmail.com>
2026-06-21 22:10:07 +0530

    49 day completed

--- day49.py
+++ day49.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 results = model.train(
-    data="data.yaml",
+    data=r"C:\Users\dell\Downloads\dataset\data.yaml",
     epochs=10,
     imgsz=640
 )
